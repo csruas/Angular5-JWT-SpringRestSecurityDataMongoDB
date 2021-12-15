c4e45f17825a1348ed51f271b34eebe839a52a7f
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2021-12-15 01:03:34 -0300

    Conhecendo  PIPES

--- first-angular-cli/src/app/app.component.ts
+++ first-angular-cli/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tasks';
- 
+  upperText:string = 'Display uppercase text';
+  lowerText:string = 'Display lowercase text';
+  percentValue: number = 0.5;
+  date: Date = new Date();
+  money: number =598;
+  
+  user: User = {
+    name: 'Cristiano',
+    age: 25
+  }
 }
