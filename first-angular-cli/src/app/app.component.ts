52856df80d28fc6efc7cef2891e07dc7a8d634c2
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2021-12-15 00:02:14 -0300

    Criando o primeiro componente

--- first-angular-cli/src/app/app.component.ts
+++ first-angular-cli/src/app/app.component.ts
@@ -7,10 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tasks';
-  tasks = [];
-  task = "";
-
-  add():void{
-    this.tasks.push(this.task);
-  }
+ 
 }
